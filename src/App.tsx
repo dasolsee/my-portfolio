870b2de2dbfb0eb9d6ab40d51b277d1dec2cb759
dasolsee <dasol_2@naver.com>
2026-09-04 11:11:43 +0900

    refactor: 네비게이션 데이터를 컴포넌트 내부로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <BrowserRouter> {/*브라우저 URL을 리액터라우터가 감지할수 있게 해줌*/}
         <Routes>
           <Route path= "/" element={<Home />} /> {/*주소가 기본 /일때 홈보여주기*/}
-          <Route path="/archive" element={<Archive />} />
+          <Route path="/archive" element={<Archive />} />{/* 얘는 주소가 /archive일때 아카이브 보여주기*/}
         </Routes>
       </BrowserRouter>
   )
--- src/components/common/Navigation/Navigation.tsx
+++ src/components/common/Navigation/Navigation.tsx
@@ -1,6 +1,27 @@
-import {navigationItems} from '../../../data/navigation'
-
 function Navigation() {
+    const navigationItems = [
+        {
+            id: 'about',
+            label: 'About',
+        },
+        {
+            id: 'experience',
+            label:'Experience',
+        },
+        {
+            id: 'skills',
+            label: 'Skills',
+        },
+        {
+            id: 'projects',
+            label: 'Projects',
+        },
+        {
+            id: 'contact',
+            label: 'Contact',
+        }
+    ]
+
     return(
         <nav aria-label="메인 메뉴">
             <ul>
